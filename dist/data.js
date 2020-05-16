77fe5d6530e89927017224176b2e7a0f2cf2da2c
lihiitz <lihiitz@gmail.com>
2020-05-16 14:27:41 +0300

    extension1 + fixing bugs

--- dist/data.js
+++ dist/data.js
@@ -3,34 +3,49 @@ class Data {
         this.cityData = []
 
     }
-    validInput(input){
-        input = input.replace(/\s+/g, '-')
-        let res = input.charAt(0).toUpperCase() + input.slice(1)
-        return res
+    async getCityByCoords(pos) {
+        const crd = pos.coords
+        const data = await $.get(`/city/${crd.latitude}/${crd.longitude}`)
+        console.log(data);
+        
+        return data
     }
     async getDataFromDB() {
-        const data = await $.get(`/cities`)
+        const data = await $.get(`/cities`)        
         this.cityData = data
         return this.cityData
     }
-    async getCityData(city) { //get one city from API, add it to cityData array and return the array
+    //get one city from API, add it to cityData array and return the array
+    async getCityData(city) { 
         const data = await $.get(`/city/${city}`)
-        const index = this.cityData.findIndex(item => item.name === city)
+        const index = this.cityData.findIndex(item => item.name === data.name)
         if (index !== -1) {//city is in array
             const id = this.cityData[index]["_id"]
-            if (id){
+            if (id) {
                 data["_id"] = id
             }
             this.cityData.splice(index, 1)
         }
-        this.cityData.push(data)
+        this.cityData.splice(index, 0, data)
         return this.cityData
     }
     async saveCity(cityName) {
         const obj = this.cityData.find(item => item.name === cityName)
         const newObj = await $.post(`/city`, obj)
-        this.cityData.splice(this.cityData.findIndex(item => item.name === cityName), 1)
-        this.cityData.push(newObj)
+        const index = this.cityData.findIndex(item => item.name === cityName)
+        this.cityData.splice(index, 1)
+        this.cityData.splice(index, 0, newObj)
+        return this.cityData
+    }
+    async updateCity(cityName){
+        const data = await $.ajax({
+            url: `/city/${cityName}`,
+            method: `PUT`,            
+        })
+        //remove old data and insert new data to local array
+        const index = this.cityData.findIndex(item => item.name === data.name)
+        this.cityData.splice(index, 1)
+        this.cityData.splice(index,0,data)
         return this.cityData
     }
     async removeCity(cityName) {
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,19 +4,44 @@ const axios = require(`axios`)
 const City = require(`../model/City`)
 
 const APIKey = "adff5ce17061cd99b676972798efc372"
-
-router.get(`/city/:cityName`, async function(req, res){
-    const city = req.params.cityName
-    let data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${APIKey}`)
+//http://api.openweathermap.org/data/2.5/weather?q=Paris&units=Metric&appid=adff5ce17061cd99b676972798efc372
+//api.openweathermap.org/data/2.5/weather?lat=48.85&lon=2.35&appid=adff5ce17061cd99b676972798efc372
+const createCityObj = function(data){
     let final = {
-        name: city,
+        name: data.data.name.replace(/\s+/g, '-'),
         temperature: data.data.main.temp,
         condition: data.data.weather[0].description,
         conditionPic: `http://openweathermap.org/img/wn/${data.data.weather[0].icon}@2x.png`,
     }
-    res.send(final)
+    return final
+}
+router.get(`/city/:lat/:lon`, async function(req, res){
+    const lat = req.params.lat
+    const lon = req.params.lon
+    let data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=Metric&appid=${APIKey}`)
+    let cityObj = createCityObj(data)
+    res.send(cityObj)
 })
-router.get(`/cities`, async function(req, res){ //return all cities that in DB
+router.get(`/city/:cityName`, async function(req, res){
+    const city = req.params.cityName
+    let data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${APIKey}`)
+    let cityObj = createCityObj(data)
+    res.send(cityObj)
+})
+//get city from API & update DB with changed city
+router.put(`/city/:cityName`, async function(req, res){
+    const cityName = req.params.cityName
+    let data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=Metric&appid=${APIKey}`) 
+    data = createCityObj(data)
+    const dataFromDB = await City.findOneAndUpdate({name: cityName}, data, {new: true})
+    if (dataFromDB){
+        res.send(dataFromDB)
+    }else{
+        res.send(data)
+    }
+})
+//return all cities that in DB
+router.get(`/cities`, async function(req, res){ 
     let cities = await City.find({})
     res.send(cities)
 })
